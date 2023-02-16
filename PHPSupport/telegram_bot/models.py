3ef94bf22c5e279baf8d285c5575a96b02e88676
ZHartem21 <zhartem21@gmail.com>
2023-02-16 17:36:58 +0300

    Update models.py

--- PHPSupport/telegram_bot/models.py
+++ PHPSupport/telegram_bot/models.py
@@ -1,3 +1,68 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Clarification(models.Model):
+    question = models.TextField('question')
+    answer = models.TextField('answer')
+    creation_date = models.DateTimeField('request creation date', auto_now=True, auto_now_add=True)
+
+
+class Request(models.Model):
+    creation_date = models.DateTimeField('request creation date', auto_now=True, auto_now_add=True)
+    title = models.CharField('title', max_length=50)
+    description = models.TextField('description')
+    price = models.IntegerField('price', blank=True, null=True)
+    estimate = models.DateTimeField('estimate', blank=True, null=True)
+    php_login = models.CharField('php login', max_length=50)
+    php_password = models.CharField('php password', max_length=50)
+    php_link = models.CharField('php link', max_length=50)
+    clarification = models.ForeignKey(Clarification, verbose_name='clarification', on_delete=models.CASCADE)
+
+    class RequestStatus(models.TextChoices):
+        PENDING = 'PNDG', ('pending')
+        OPEN = 'OPEN', ('open')
+        WORKING = 'WORK', ('in working')
+        COMPLETE = 'CMPL', ('complete')
+        CANCELLED = 'CNLD', ('cancelled')
+
+    status = models.CharField(
+        max_length=4,
+        choices=RequestStatus.choices,
+        default=RequestStatus.PENDING,
+    )
+
+    class RequestDifficulty(models.TextChoices):
+        EASY = 'EASY', ('easy')
+        MEDIUM = 'MEDM', ('medium')
+        HARD = 'HARD', ('hard')
+
+    status = models.CharField(
+        max_length=4,
+        choices=RequestDifficulty.choices,
+    )
+
+
+class Client(models.Model):
+    name = models.CharField('name', max_length=50)
+    telegram_id = models.CharField('telegram id', max_length=50)
+    registration_date = models.DateTimeField('date of registration', auto_now=True, auto_now_add=True)
+    subscription_end = models.DateTimeField('end of subscription date')
+    request = models.ForeignKey(Request, on_delete=models.CASCADE, verbose_name='client request', null=True, blank=True)
+
+    @property
+    def is_active(self):
+        if datetime.now() < self.subscription_end:
+            return True
+        else:
+            return False
+
+
+class Subcontractor(models.Model):
+    name = models.CharField('name', max_length=50)
+    telegram_id = models.CharField('telegram id', max_length=50)
+    registration_date = models.DateTimeField('date of registration', auto_now=True, auto_now_add=True)
+    salary = models.IntegerField('salary', blank=True, null=True)
+    is_active = models.BooleanField('is active', blank=True, null=True)
+    request = models.ForeignKey(Request, on_delete=models.SET_NULL, verbose_name='subcontractor request', null=True, blank=True)
